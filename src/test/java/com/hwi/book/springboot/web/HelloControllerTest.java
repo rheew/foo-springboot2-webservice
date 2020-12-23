c76845bb865f668940b23a0adf2fc7aebea5bff9
rheew <47098896+rheew@users.noreply.github.com>
2020-12-23 16:28:17 +0900

    google login and Mysql session

--- src/test/java/com/hwi/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/hwi/book/springboot/web/HelloControllerTest.java
@@ -7,12 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
--- src/test/java/com/hwi/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hwi/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -6,7 +6,6 @@ import com.hwi.book.springboot.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
